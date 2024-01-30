6f034fa172a76400c33dda87ec7b38e408f86e53
gaalpos <gabripazos13@gmail.com>
2024-01-30 11:56:18 +0100

    test to see if i need to deploy again

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1> hello world!</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p> Test</p>
       </header>
     </div>
   );
